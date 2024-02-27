3839d343fe552fd7d75f93e027003ff728a9d6b4
Nora <mwdrad@gmail.com>
2024-02-27 19:52:19 +0330

    fix variables - remove body and other elements styles

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const htmlElement = document.documentElement;
-
-    // Check if the user's system prefers dark mode
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    // Add or remove the 'dark-theme' class based on the preference
     if (prefersDarkMode) {
         htmlElement.classList.add('dark-theme');
         htmlElement.classList.remove('light-theme');
@@ -12,4 +8,34 @@ document.addEventListener('DOMContentLoaded', function () {
         htmlElement.classList.remove('dark-theme');
         htmlElement.classList.add('light-theme');
     }
+
+
+    const chatboxContent = document.querySelector('.wpsms-chatbox__content');
+    const toggleButton = document.querySelector('.js-wpsms-chatbox__button');
+    const closeButton = document.querySelector('.js-wpsms-chatbox__close-button');
+
+    toggleButton.addEventListener('click', function () {
+        if (chatboxContent.classList.contains('open')) {
+            chatboxContent.classList.remove('open');
+            chatboxContent.classList.add('closing');
+        } else {
+            chatboxContent.style.display = 'block'; // Show the content before animating
+            chatboxContent.classList.add('open', 'opening');
+        }
+        document.body.classList.toggle('chatbox-open');
+    });
+    closeButton.addEventListener('click', function () {
+        chatboxContent.classList.remove('open');
+        chatboxContent.classList.add('closing');
+    });
+    // Reset animations and hide content on transition end
+    chatboxContent.addEventListener('transitionend', function (event) {
+        if (event.propertyName === 'opacity' && !chatboxContent.classList.contains('open')) {
+            chatboxContent.style.display = 'none';
+        }
+        chatboxContent.classList.remove('opening', 'closing');
+    });
+
+
+
 });
